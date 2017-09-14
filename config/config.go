e571eb1d37cbc17fef866a9db5f6d5f537797ae6
Evan Smith <esincork@gmail.com>
2017-09-15 00:34:52 +0100

    Abstract config to subpackage
    This allows us to access the config from other subpackages such as the command subpackage

---
+++ config/config.go
@@ -0,0 +1,63 @@
+package config
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io/ioutil"
+)
+
+var (
+	conf *Config
+)
+
+// config represetns the bot configuration loaded from the JSON
+// file "./config.json".
+type Config struct {
+	// Prefix is the string that will prefix all commands
+	// which this not will listen for.
+	Prefix string `json:"prefix"`
+	// Token is the Discord bot user token.
+	Token string `json:"token"`
+	// HelpChannelID is the channel ID to which help messages from
+	// netsoc-admin will be sent.
+	HelpChannelID string `json:"helpChannelID"`
+	// BotHostName is the address which the bot can be reached at
+	// over the internet. This is used by netsocadmin to reach the
+	// '/help' endpoint.
+	BotHostName string `json:"botHostName"`
+	// SysAdminTag is the tag which, when included in a disocrd message,
+	// will result in a notification being sent to all SysAdmins so they
+	// can be notified of the help message.
+	GuildID     string `json:"guildID"`
+	SysAdminTag string `json:"sysAdminTag"`
+
+	Permissions Permissions `json:"permissions"`
+}
+
+type Permissions struct {
+	Set []string `json:"set"`
+}
+
+// loadConfig loads the configuration information found in ./config.json
+func LoadConfig() error {
+	file, err := ioutil.ReadFile("config.json")
+	if err != nil {
+		return fmt.Errorf("failed to read configuration file: %v", err)
+	}
+
+	if len(file) < 1 {
+		return errors.New("Configuration file 'config.json' was empty")
+	}
+
+	conf = &Config{}
+	if err := json.Unmarshal(file, conf); err != nil {
+		return fmt.Errorf("failed to unmarshal configuration JSON: %s", err)
+	}
+
+	return nil
+}
+
+func GetConfig() *Config {
+	return conf
+}
